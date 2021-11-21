79850ad88d646f70796261043aeeb6438db1bf54
Aslam Maududy <aslammaududy@outlook.com>
2021-11-21 14:47:22 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/satsiber/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.satsiber
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,37 @@
+import 'package:contained_tab_bar_view/contained_tab_bar_view.dart';
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Satsiber'),
+        centerTitle: false,
+        elevation: 0,
+      ),
+      body: ContainedTabBarView(
+        tabs: const [
+          Text("CHATS"),
+          Text("GROUPS"),
+          Text("PROFILE"),
+        ],
+        tabBarProperties: TabBarProperties(
+          background: Container(
+            color: Colors.blueAccent,
+          ),
+          indicatorColor: Colors.white,
+        ),
+        views: const [
+          Center(child: Text("Chats View")),
+          Center(child: Text("Groups View")),
+          Center(child: Text("Profile View"))
+        ],
+      ),
+    );
+  }
+}
